7154cd828292b6a9a88ab3801a495f63a71131bc
Moaaz Mustafa <101462341+MoaazMustafa@users.noreply.github.com>
2026-05-09 14:13:00 +0500

    feat: integrate PrismaAdapter and configure JWT session strategy in authOptions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+  session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
